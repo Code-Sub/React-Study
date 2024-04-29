ed5c3141dcfa0153013dfbb6d879d30f2f6240e3
Code-Sub <asdf-1980@naver.com>
2024-04-29 15:34:03 +0900

    useEffect

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -6,23 +6,42 @@ export default function DayList() {
     const [days, setDays] = useState([]);
     const [count, setCount] = useState(0);
     
+    
     function onClick() {
         setCount(count +1)
+    }
+    // 이전의 days 배열은 더 이상 사용되지 않으면 가비지 컬렉션의 대상이 된다
+    function onClick2() {
+        days.push({
+            id: Math.random(),
+            day:1
+        })
+        setDays(days)
+        // setDays([
+        //     ...days, {
+        //         id: Math.random(),
+        //         day: 1
+        //     }
+        // ])
     }
     
     useEffect(()=> {
         console.log("Count change");
-    })
+        
+    },[count])
+    
+    
     return (
         <>
             <ul className='list_day'>
-                {dummy.days.map(day => (
+                {days.map(day => (
                     <li key={day.id}>
                         <Link to={`/day/${day.day}`}>Day {day.day}</Link>
                     </li>
                 ))}
             </ul>
             <button onClick={onClick}>{count}</button>
+            <button onClick={onClick2}>Day Chage</button>
         </>
     );
 }
